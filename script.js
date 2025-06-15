b5a6a735fb3a650c58b9685621e0a034b6c820a4
mdahmed4875 <140884439+mdahmed4875@users.noreply.github.com>
2025-06-15 11:05:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,33 +4,33 @@ let output=document.getElementById('output');
 btn.addEventListener('click',()=>{
 	new Promise((resolve)=>{
 		setTimeout(()=>{
+			output.textContent=`Result:${number.value}`;
 			resolve(number.value)
 		},2000)
 	}).then((number)=>{
-	     output.textContent=number;
 		return new Promise((resolve)=>{
 			setTimeout(()=>{
-				output.textContent=number*2;
+				output.textContent=`Result:${number*2}`;
 				resolve(number*2);
 			},2000)
 		})
 	}).then((number)=>{
 		return new Promise((resolve)=>{
 			setTimeout(()=>{
-				output.textContent=number-3;
+				output.textContent=`Result:${number-3}`;
 				resolve(number-3);
 			},1000)
 		})
 	}).then((number)=>{
 		return new Promise((resolve)=>{
 			setTimeout(()=>{
-				output.textContent=number/2;
+				output.textContent=`Result:${number/2}`;
 				resolve(number/2);
 			},1000)
 		})
 	}).then((number)=>{
 		setTimeout(()=>{
-			output.textContent=number+10;
+			output.textContent=`Final Result:${number+10}`;
 		},1000)
 	})
 })
